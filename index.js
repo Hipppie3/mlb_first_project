19879615871926a3ef7f5060f1638d17227013fa
Daniel Hiep Truong <danieltruong89@yahoo.com>
2022-10-06 09:53:13 -0600

    update with 2 event listener and fetch

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
- //Variables for each li
+// Variables for each li
 const left = document.getElementById('left-wing')
 const center = document.getElementById('center')
 const right = document.getElementById('right-wing')
+
 // Variables for 3 left wing players
   //Carl Hagelin
     let img1 = document.createElement('img')
@@ -12,6 +13,8 @@ const right = document.getElementById('right-wing')
   //Marcus Johansson
     let img3 = document.createElement('img')
     img3.src = "https://media2.giphy.com/media/3otOKoCoW8jXXzftwQ/giphy.gif?cid=ecf05e4791pxh45buvh6acnlqmcw04olvwn5yo36z7l7nxax&rid=giphy.gif&ct=g"
+
+
 //Variables for 3 center players
   // Nic Dowd
     let img1f = document.createElement('img')
@@ -22,6 +25,7 @@ const right = document.getElementById('right-wing')
   //Evgeny Kuznetsov
     let img3f = document.createElement('img')
     img3f.src = "https://media2.giphy.com/media/VFZSfV6EmxFQ1dUyHi/200w.webp?cid=ecf05e47atq523z3n3bfhv8v0zhdw6g47hwncw41wj438tbv&rid=200w.webp&ct=g"
+
 //Variables for 3 right wing players
   //T.J. Oshie
     let img1rw = document.createElement('img')
@@ -32,20 +36,27 @@ const right = document.getElementById('right-wing')
   //Garnet Hathaway
     let img3rw = document.createElement('img')
     img3rw.src = "https://media3.giphy.com/media/MF1ueXGihs4UyuRazn/200w.webp?cid=ecf05e47yfjy867u9z4gwoohfny3ex5b77dygaph62ai30eb&rid=200w.webp&ct=g"
+
+  //Red PIll
+
+
 //Assigning each frontImage to a variable
   let firstCardImg = document.getElementById('frontImage1')
   let secondCardImg = document.getElementById('frontImage2')
   let thirdCardImg = document.getElementById('frontImage3')
+
+
 // Function for each li to replace image
- // Replacing leftWings
+//  Replacing leftWings
   function leftWingPlayers() {
    left.addEventListener('click', (e) => {
    e.preventDefault()
     firstCardImg.src = img1.src;
     secondCardImg.src = img2.src;
     thirdCardImg.src = img3.src;
-   //  backstromDetails()
 })}
+
+
  //Replacing centers
   function centerPlayers() {
    center.addEventListener('click', (e) => {
@@ -54,224 +65,320 @@ const right = document.getElementById('right-wing')
     secondCardImg.src = img2f.src;
     thirdCardImg.src = img3f.src;
 })}
- //Replacing rightWings
- function rightWingPlayers() {
-   right.addEventListener('click', (e) => {
-   e.preventDefault()
+//  Replacing rightWings
+//  function rightWingPlayers() {
+//    right.addEventListener('click', (e) => {
+//    e.preventDefault()
+//     firstCardImg.src = img1rw.src;
+//     secondCardImg.src = img2rw.src;
+//     thirdCardImg.src = img3rw.src;
+// })}
+
+// // Invoking the functions
+// leftWingPlayers();
+// centerPlayers();
+// rightWingPlayers(); 
+
+
+
+//Create an onClick for each children under the parent(#navigation) li
+//Once clicked, a new image of that position will appear in each card from parent(.allCards) 
+
+
+
+
+
+// Right Wing
+fetch("https://statsapi.web.nhl.com/api/v1/people/8476880")
+.then(res => res.json())
+.then(data => 
+tom(data))
+
+
+
+const firstDes = document.querySelector('.position1')
+function tom(data) {
+right.addEventListener('click', (e) => {
+   e.preventDefault() 
+const firstPara = document.getElementById('firstP')
+const firstParap = document.getElementById('firstPP')
+const firstParapp = document.getElementById('firstPPP')
+const firstParappp = document.getElementById('firstPPPP')
+const firstName = data.people[0]
+firstPara.textContent = `${'Full Name: '} ${firstName.fullName}`
+firstParap.textContent = `${'Birth Date: '} ${firstName.birthDate}`
+firstParapp.textContent = `${'Nationality: '} ${firstName.nationality}`
+firstParappp.textContent = `${'Number: '} ${firstName.primaryNumber}`
+     firstCardImg.src = img1rw.src;
+    secondCardImg.src = img2rw.src;
+    thirdCardImg.src = img3rw.src;;
+})}
+
+fetch("https://statsapi.web.nhl.com/api/v1/people/8471698")
+.then(res => res.json())
+.then(data => 
+tj(data))
+
+const secondDes = document.querySelector('.position2')
+function tj(data) {
+right.addEventListener('click', (e) => {
+   e.preventDefault() 
+const secondPara = document.getElementById('secondP')
+const secondParap = document.getElementById('secondPP')
+const secondParapp= document.getElementById('secondPPP')
+const secondParappp = document.getElementById('secondPPPP')
+const firstName = data.people[0]
+secondPara.textContent = `${'Full Name: '} ${firstName.fullName}`
+secondParap.textContent = `${'Birth Date: '} ${firstName.birthDate}`
+secondParapp.textContent = `${'Nationality: '} ${firstName.nationality}`
+secondParappp.textContent = `${'Number: '} ${firstName.primaryNumber}`
+     firstCardImg.src = img1.src;
+     firstCardImg.src = img1rw.src;
+    secondCardImg.src = img2rw.src;
+    thirdCardImg.src = img3rw.src;;
+})}
+
+fetch("https://statsapi.web.nhl.com/api/v1/people/8477903")
+.then(res => res.json())
+.then(data => 
+garnet(data))
+
+const thirdDes = document.querySelector('.position3')
+function garnet(data) {
+right.addEventListener('click', (e) => {
+   e.preventDefault() 
+const thirdPara = document.getElementById('thirdP')
+const thirdParap = document.getElementById('thirdPP')
+const thirdParapp = document.getElementById('thirdPPP')
+const thirdParappp = document.getElementById('thirdPPPP')
+const firstName = data.people[0]
+thirdPara.textContent = `${'Full Name: '} ${firstName.fullName}`
+thirdParap.textContent = `${'Birth Date: '} ${firstName.birthDate}`
+thirdParapp.textContent = `${'Nationality: '} ${firstName.nationality}`
+thirdParappp.textContent = `${'Number: '} ${firstName.primaryNumber}`
     firstCardImg.src = img1rw.src;
     secondCardImg.src = img2rw.src;
-    thirdCardImg.src = img3rw.src;
+    thirdCardImg.src = img3rw.src;;
 })}
-//Invoking the functions
-leftWingPlayers();
-centerPlayers();
-rightWingPlayers();
 
 
-const CarlHagelin = "https://statsapi.web.nhl.com/api/v1/people/8474176"
-const nicklasBackstrom = "https://statsapi.web.nhl.com/api/v1/people/8473563"
-const tomWilson = "https://statsapi.web.nhl.com/api/v1/people/8476880"
-const alexOvechkin = "https://statsapi.web.nhl.com/api/v1/people/8471214"
-const tjOshie = "https://statsapi.web.nhl.com/api/v1/people/8471698"
-const larsEller= "https://statsapi.web.nhl.com/api/v1/people/8474189"
-const marcusJohannson = "https://statsapi.web.nhl.com/api/v1/people/8475149"
-const nicDowd = "https://statsapi.web.nhl.com/api/v1/people/8475343"
-const garnetHathaway = "https://statsapi.web.nhl.com/api/v1/people/8477903"
 
-fetch(CarlHagelin)
+
+//Center
+fetch("https://statsapi.web.nhl.com/api/v1/people/8475343")
 .then(res => res.json())
-.then(data =>{
-   console.log(data.people[0])
-   hagelinDetails(data.people[0])
-})
+.then(data => 
+nic(data))
 
-fetch(nicklasBackstrom)
+const firstsDes = document.querySelector('.position1')
+function nic(data) {
+center.addEventListener('click', (e) => {
+   e.preventDefault() 
+const firstPara = document.getElementById('firstP')
+const firstParap = document.getElementById('firstPP')
+const firstParapp = document.getElementById('firstPPP')
+const firstParappp = document.getElementById('firstPPPP')
+const firstName = data.people[0]
+firstPara.textContent = `${'Full Name: '} ${firstName.fullName}`
+firstParap.textContent = `${'Birth Date: '} ${firstName.birthDate}`
+firstParapp.textContent = `${'Nationality: '} ${firstName.nationality}`
+firstParappp.textContent = `${'Number: '} ${firstName.primaryNumber}`
+    firstCardImg.src = img1f.src;
+    secondCardImg.src = img2f.src;
+    thirdCardImg.src = img3f.src;;
+})}
+
+fetch("https://statsapi.web.nhl.com/api/v1/people/8474189")
 .then(res => res.json())
-.then(data =>{
-   console.log(data.people[0])
-   backstromDetails(data.people[0])
-   //playerDetails(data.people[0])
-})
+.then(data => 
+lars(data))
 
-fetch(tomWilson)
+const secondsDes = document.querySelector('.position2')
+function lars(data) {
+center.addEventListener('click', (e) => {
+   e.preventDefault() 
+const secondPara = document.getElementById('secondP')
+const secondParap = document.getElementById('secondPP')
+const secondParapp= document.getElementById('secondPPP')
+const secondParappp = document.getElementById('secondPPPP')
+const firstName = data.people[0]
+secondPara.textContent = `${'Full Name: '} ${firstName.fullName}`
+secondParap.textContent = `${'Birth Date: '} ${firstName.birthDate}`
+secondParapp.textContent = `${'Nationality: '} ${firstName.nationality}`
+secondParappp.textContent = `${'Number: '} ${firstName.primaryNumber}`
+     firstCardImg.src = img1.src;
+     firstCardImg.src = img1f.src;
+    secondCardImg.src = img2f.src;
+    thirdCardImg.src = img3f.src;;
+})}
+
+fetch("https://statsapi.web.nhl.com/api/v1/people/8475744")
 .then(res => res.json())
-.then(data =>{
-   console.log(data.people[0])
-   wilsonDetails(data.people[0])
-   //playerDetails(data.people[0])
-})
+.then(data => 
+ev(data))
 
-fetch(alexOvechkin)
+const thirdsDes = document.querySelector('.position3')
+function ev(data) {
+center.addEventListener('click', (e) => {
+   e.preventDefault() 
+const thirdPara = document.getElementById('thirdP')
+const thirdParap = document.getElementById('thirdPP')
+const thirdParapp = document.getElementById('thirdPPP')
+const thirdParappp = document.getElementById('thirdPPPP')
+const firstName = data.people[0]
+thirdPara.textContent = `${'Full Name: '} ${firstName.fullName}`
+thirdParap.textContent = `${'Birth Date: '} ${firstName.birthDate}`
+thirdParapp.textContent = `${'Nationality: '} ${firstName.nationality}`
+thirdParappp.textContent = `${'Number: '} ${firstName.primaryNumber}`
+     firstCardImg.src = img1f.src;
+    secondCardImg.src = img2f.src;
+    thirdCardImg.src = img3f.src;;
+})}
+
+
+// Left Wing
+
+fetch("https://statsapi.web.nhl.com/api/v1/people/8474176")
 .then(res => res.json())
-.then(data =>{
-   console.log(data.people[0])
-   ovechkinDetails(data.people[0])
-   //playerDetails(data.people[0])
-})
+.then(data => 
+carl(data))
 
-fetch(tjOshie)
+const firstyDes = document.querySelector('.position1')
+function carl(data) {
+left.addEventListener('click', (e) => {
+   e.preventDefault() 
+const firstPara = document.getElementById('firstP')
+const firstParap = document.getElementById('firstPP')
+const firstParapp = document.getElementById('firstPPP')
+const firstParappp = document.getElementById('firstPPPP')
+const firstName = data.people[0]
+firstPara.textContent = `${'Full Name: '} ${firstName.fullName}`
+firstParap.textContent = `${'Birth Date: '} ${firstName.birthDate}`
+firstParapp.textContent = `${'Nationality: '} ${firstName.nationality}`
+firstParappp.textContent = `${'Number: '} ${firstName.primaryNumber}`
+     firstCardImg.src = img1.src;
+    secondCardImg.src = img2.src;
+    thirdCardImg.src = img3.src;;
+})}
+
+fetch("https://statsapi.web.nhl.com/api/v1/people/8471214")
 .then(res => res.json())
-.then(data =>{
-   console.log(data.people[0])
-   oshieDetails(data.people[0])
-   //playerDetails(data.people[0])
-})
+.then(data => 
+alex(data))
 
-fetch(larsEller)
+const secondyDes = document.querySelector('.position2')
+function alex(data) {
+left.addEventListener('click', (e) => {
+   e.preventDefault() 
+const secondPara = document.getElementById('secondP')
+const secondParap = document.getElementById('secondPP')
+const secondParapp= document.getElementById('secondPPP')
+const secondParappp = document.getElementById('secondPPPP')
+const firstName = data.people[0]
+secondPara.textContent = `${'Full Name: '} ${firstName.fullName}`
+secondParap.textContent = `${'Birth Date: '} ${firstName.birthDate}`
+secondParapp.textContent = `${'Nationality: '} ${firstName.nationality}`
+secondParappp.textContent = `${'Number: '} ${firstName.primaryNumber}`
+     firstCardImg.src = img1.src;
+    secondCardImg.src = img2.src;
+    thirdCardImg.src = img3.src;;
+})}
+
+fetch("https://statsapi.web.nhl.com/api/v1/people/8475149")
 .then(res => res.json())
-.then(data =>{
-   console.log(data.people[0])
-   ellerDetails(data.people[0])
-   //playerDetails(data.people[0])
-})
+.then(data => 
+marc(data))
 
-fetch(marcusJohannson)
-.then(res => res.json())
-.then(data =>{
-   console.log(data.people[0])
-   johannsonDetails(data.people[0])
-   //playerDetails(data.people[0])
-})
-
-fetch(nicDowd)
-.then(res => res.json())
-.then(data =>{
-   console.log(data.people[0])
-   dowdDetails(data.people[0])
-   //playerDetails(data.people[0])
-})
-
-fetch(garnetHathaway)
-.then(res => res.json())
-.then(data =>{
-   console.log(data.people[0])
-   hathawayDetails(data.people[0])
-   //playerDetails(data.people[0])
-})
-
-fetch("https://statsapi.web.nhl.com/api/v1/teams/15?expand=team.roster")
-.then(res => res.json())
-.then(data => {
-   console.log(data)
-}
-)
-
-function hagelinDetails(playersObj) {
-   const position = document.querySelector(".position")
-   const firstName = document.querySelector(".name")
-   const number = document.querySelector(".number")
-   const age = document.querySelector(".birthday")
-   const country = document.querySelector(".country")
-   position.innerHTML = playersObj.primaryPosition.name
-   firstName.innerHTML = playersObj.fullName
-   number.innerHTML = playersObj.primaryNumber
-   age.innerHTML = playersObj.birthDate
-   country.innerHTML = playersObj.birthCountry
-}
-
-function backstromDetails(playersObj) {
-   const position = document.querySelector(".position1")
-   const firstName = document.querySelector(".name1")
-   const number = document.querySelector(".number1")
-   const age = document.querySelector(".birthday1")
-   const country = document.querySelector(".country1")
-   position.innerHTML = playersObj.primaryPosition.name
-   firstName.innerHTML = playersObj.fullName
-   number.innerHTML = playersObj.primaryNumber
-   age.innerHTML = playersObj.birthDate
-   country.innerHTML = playersObj.birthCountry
-}
-
-function wilsonDetails(playersObj) {
-   const position = document.querySelector(".position2")
-   const firstName = document.querySelector(".name2")
-   const number = document.querySelector(".number2")
-   const age = document.querySelector(".birthday2")
-   const country = document.querySelector(".country2")
-   position.innerHTML = playersObj.primaryPosition.name
-   firstName.innerHTML = playersObj.fullName
-   number.innerHTML = playersObj.primaryNumber
-   age.innerHTML = playersObj.birthDate
-   country.innerHTML = playersObj.birthCountry
-}
-
-function ovechkinDetails(playersObj) {
-   const position = document.querySelector(".position3")
-   const firstName = document.querySelector(".name3")
-   const number = document.querySelector(".number3")
-   const age = document.querySelector(".birthday3")
-   const country = document.querySelector(".country3")
-   position.innerHTML = playersObj.primaryPosition.name
-   firstName.innerHTML = playersObj.fullName
-   number.innerHTML = playersObj.primaryNumber
-   age.innerHTML = playersObj.birthDate
-   country.innerHTML = playersObj.birthCountry
-}
-
-function oshieDetails(playersObj) {
-   const position = document.querySelector(".position4")
-   const firstName = document.querySelector(".name4")
-   const number = document.querySelector(".number4")
-   const age = document.querySelector(".birthday4")
-   const country = document.querySelector(".country4")
-   position.innerHTML = playersObj.primaryPosition.name
-   firstName.innerHTML = playersObj.fullName
-   number.innerHTML = playersObj.primaryNumber
-   age.innerHTML = playersObj.birthDate
-   country.innerHTML = playersObj.birthCountry
-}
-
-function ellerDetails(playersObj) {
-   const position = document.querySelector(".position5")
-   const firstName = document.querySelector(".name5")
-   const number = document.querySelector(".number5")
-   const age = document.querySelector(".birthday5")
-   const country = document.querySelector(".country5")
-   position.innerHTML = playersObj.primaryPosition.name
-   firstName.innerHTML = playersObj.fullName
-   number.innerHTML = playersObj.primaryNumber
-   age.innerHTML = playersObj.birthDate
-   country.innerHTML = playersObj.birthCountry
-}
-
-function johannsonDetails(playersObj) {
-   const position = document.querySelector(".position6")
-   const firstName = document.querySelector(".name6")
-   const number = document.querySelector(".number6")
-   const age = document.querySelector(".birthday6")
-   const country = document.querySelector(".country6")
-   position.innerHTML = playersObj.primaryPosition.name
-   firstName.innerHTML = playersObj.fullName
-   number.innerHTML = playersObj.primaryNumber
-   age.innerHTML = playersObj.birthDate
-   country.innerHTML = playersObj.birthCountry
-}
-
-function dowdDetails(playersObj) {
-   const position = document.querySelector(".position7")
-   const firstName = document.querySelector(".name7")
-   const number = document.querySelector(".number7")
-   const age = document.querySelector(".birthday7")
-   const country = document.querySelector(".country7")
-   position.innerHTML = playersObj.primaryPosition.name
-   firstName.innerHTML = playersObj.fullName
-   number.innerHTML = playersObj.primaryNumber
-   age.innerHTML = playersObj.birthDate
-   country.innerHTML = playersObj.birthCountry
-}
-
-function hathawayDetails(playersObj) {
-   const position = document.querySelector(".position8")
-   const firstName = document.querySelector(".name8")
-   const number = document.querySelector(".number8")
-   const age = document.querySelector(".birthday8")
-   const country = document.querySelector(".country8")
-   position.innerHTML = playersObj.primaryPosition.name
-   firstName.innerHTML = playersObj.fullName
-   number.innerHTML = playersObj.primaryNumber
-   age.innerHTML = playersObj.birthDate
-   country.innerHTML = playersObj.birthCountry
-}
+const thirdyDes = document.querySelector('.position3')
+function marc(data) {
+left.addEventListener('click', (e) => {
+   e.preventDefault() 
+const thirdPara = document.getElementById('thirdP')
+const thirdParap = document.getElementById('thirdPP')
+const thirdParapp = document.getElementById('thirdPPP')
+const thirdParappp = document.getElementById('thirdPPPP')
+const firstName = data.people[0]
+thirdPara.textContent = `${'Full Name: '} ${firstName.fullName}`
+thirdParap.textContent = `${'Birth Date: '} ${firstName.birthDate}`
+thirdParapp.textContent = `${'Nationality: '} ${firstName.nationality}`
+thirdParappp.textContent = `${'Number: '} ${firstName.primaryNumber}`
+     firstCardImg.src = img1.src;
+    secondCardImg.src = img2.src;
+    thirdCardImg.src = img3.src;;
+})}
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//right wing fetch
+// const fetchTom = fetch("https://statsapi.web.nhl.com/api/v1/people/8471698")
+// .then(res => res.json())
+// const fetchTJ = fetch("https://statsapi.web.nhl.com/api/v1/people/8476880")
+// .then(res => res.json())
+// const fetchGarnet = fetch("https://statsapi.web.nhl.com/api/v1/people/8477903")
+// .then(res => res.json())
+
+//center
+// const fetchNic = fetch("https://statsapi.web.nhl.com/api/v1/people/8475343")
+// .then(res => res.json())
+// const fetchLars = fetch("https://statsapi.web.nhl.com/api/v1/people/8474189")
+// .then(res => res.json())
+// const fetchEv = fetch("https://statsapi.web.nhl.com/api/v1/people/8475744")
+// .then(res => res.json())
+
+//left wing
+// const fetchCarl = fetch("https://statsapi.web.nhl.com/api/v1/people/8474176")
+// .then(res => res.json())
+// const fetchAlex = fetch("https://statsapi.web.nhl.com/api/v1/people/8471214")
+// .then(res => res.json())
+// const fetchMarcus = fetch("https://statsapi.web.nhl.com/api/v1/people/8475149")
+// .then(res => res.json())
